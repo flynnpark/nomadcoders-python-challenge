f5452d38221eeea33c13d5a802e5aea53e7db6d5
Flynn <wphestiraid@gmail.com>
2020-04-16 22:24:25 +0900

    Fix error at Day 04
    - Format code

--- src/day04.py
+++ src/day04.py
@@ -12,49 +12,48 @@ def check_down(urls: List[str]) -> None:
     for process in processes:
         process.join()
 
-def check_url(url: str) -> None:
-    url = url.replace(" ", "").lower()
 
-    if not '.' in url:
+def check_url(url: str) -> None:
+    if not "." in url:
         print(f"{url} is not a valid url.")
         return
 
-    if not 'http://' in url:
-        url = f'http://{url}'
+    url = url.replace(" ", "").lower()
+    if not url.startswith("http://"):
+        url = f"http://{url}"
 
     try:
         response = requests.get(url)
         if response.status_code == 200:
             print(f"{url} is up!")
-        
+
         else:
             print(f"{url} is down!")
-    
+
     except ConnectionError:
         print(f"{url} is down!")
 
 
 def clear() -> None:
-    if os.name == 'nt':
-        os.system('cls')
-    
+    if os.name == "nt":
+        os.system("cls")
     else:
-        os.system('clear')
+        os.system("clear")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     while True:
         print("Welcome to IsItDown.py!")
         print("Please write a URL or URLs you want to check. (separated by comma)")
         input_value = input()
-        urls = input_value.split(',')
+        urls = input_value.split(",")
 
         check_down(urls)
-        
+
         while True:
             is_over = input("Do you want to start over? (y/n) ")
-            if is_over in ['y', 'n']:
-                if is_over == 'n':
+            if is_over in ["y", "n"]:
+                if is_over == "n":
                     exit(0)
                 clear()
                 break
